a0cf9a8baf1577c70e142bb0e2c1235fd88381b1
zzlyung <124063972+zzlyung@users.noreply.github.com>
2025-05-24 13:07:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone https://github.com/281677160/openwrt-package package/openwrt-package
