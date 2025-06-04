6f47a9ae44cb0fd468f21bb5e3c5532fd017a486
zzlyung <124063972+zzlyung@users.noreply.github.com>
2025-06-04 12:18:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,17 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+sed -i '$a src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' ./feeds.conf.default
+
+git clone --depth=1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
+git clone https://github.com/vernesong/OpenClash package/OpenClash
+git clone https://github.com/xiaorouji/openwrt-passwall2 package/openwrt-passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
+
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+git clone https://github.com/BCYDTZ/luci-app-UUGameAcc package/luci-app-UUGameAcc
+git clone https://github.com/Droid-MAX/luci-app-lingtigamebooster package/luci-app-lingtigamebooster
+git clone https://github.com/lisaac/luci-app-diskman package/luci-app-diskman
+git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
