0dc476377b0db7c55d92482cbc9fae0bd22e6a19
zzlyung <124063972+zzlyung@users.noreply.github.com>
2025-06-04 18:10:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-sed -i '$a src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' ./feeds.conf.default
+#sed -i '$a src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' ./feeds.conf.default
 
 git clone --depth=1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
 git clone https://github.com/vernesong/OpenClash package/OpenClash
@@ -28,4 +28,3 @@ git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-ap
 git clone https://github.com/BCYDTZ/luci-app-UUGameAcc package/luci-app-UUGameAcc
 git clone https://github.com/Droid-MAX/luci-app-lingtigamebooster package/luci-app-lingtigamebooster
 git clone https://github.com/lisaac/luci-app-diskman package/luci-app-diskman
-git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
